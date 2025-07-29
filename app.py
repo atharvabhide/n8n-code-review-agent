e7eeafc697917515b8ee4e249b0dd80c077cd16b
Atharva Bhide <67187699+atharvabhide@users.noreply.github.com>
2025-07-29 22:17:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+  return {
+    "message": "hello server"
+  }
+
+if __name__ == "__main__":
+  uvicorn.run("app:app", reload=True)
